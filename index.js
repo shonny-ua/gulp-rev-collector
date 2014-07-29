cc91a9472007fe0f0b0e21e251a00ea548468b5b
Oleksander Ovcharenko <shonny.ua@gmail.com>
2014-07-29 18:59:25 +0300

    0.0.8 draft

--- index.js
+++ index.js
@@ -7,21 +7,43 @@ var path        = require('path');
 
 var PLUGIN_NAME = 'gulp-rev-collector';
 
+function _getManifestData(file) {
+    var data;
+    var ext = path.extname(file.path);
+    if (ext === '.json') {
+        var json = {};
+        try {
+            json = JSON.parse(file.contents.toString('utf8'))
+        } catch (x) {
+            this.emit('error', new PluginError(PLUGIN_NAME,  x));
+            return;
+        }
+        if (_.isObject(json)) {
+            var isRev = 1;
+            Object.keys(json).forEach(function (key) {
+                if ( path.basename(json[key]).replace(/-[0-9a-f]{8}-?/, '' ) !==  path.basename(key) ) {
+                    isRev = 0;
+                }
+            });
+            
+            if (isRev) {
+                data = json;
+            }
+        }
+        
+    }
+    return data;
+}
+
 function revCollector() {
     var manifest  = {};
     var mutables = [];
     return through.obj(function (file, enc, cb) {
         if (!file.isNull()) {
-            var ext = path.extname(file.path);
-            if (ext === '.json') {
-                var json = {};
-                try {
-                    json = JSON.parse(file.contents.toString('utf8'))
-                } catch (x) {
-                    this.emit('error', new PluginError(PLUGIN_NAME,  x));
-                }
-                _.extend( manifest, json );
-            } else if (~['.js', '.css', '.html', '.htm', '.phtml', '.shtml', '.php', '.erb'].indexOf(ext)) {
+            var mData = _getManifestData(file);
+            if (mData) {
+                _.extend( manifest, mData );
+            } else {
                 mutables.push(file);
             }
         }
